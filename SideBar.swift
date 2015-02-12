1eec487bf289b2714a6e0915815f5140ec23e8c1
joelkelly <joel.kelly@me.com>
2015-02-12 07:27:57 -0800

    Updating .gitignore

---
+++ SideBar.swift
@@ -0,0 +1,13 @@
+//
+//  SideBar.swift
+//  psychic
+//
+//  Created by Joel Kelly on 2/11/15.
+//  Copyright (c) 2015 Sleepy Mongoose. All rights reserved.
+//
+
+import UIKit
+
+class SideBar: NSObject {
+   
+}
